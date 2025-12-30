67df93ee565c65f9fd27503b4e6eb40cb5e4a661
dan_the_3rd <43445237+danthe3rd@users.noreply.github.com>
2025-12-30 11:24:32 +0100

    Update flash_attn_interface.py

--- flash_attn/flash_attn_interface.py
+++ flash_attn/flash_attn_interface.py
@@ -12,7 +12,10 @@ USE_TRITON_ROCM = os.getenv("FLASH_ATTENTION_TRITON_AMD_ENABLE", "FALSE") == "TR
 if USE_TRITON_ROCM:
     from aiter.ops.triton._triton_kernels.flash_attn_triton_amd import flash_attn_2 as flash_attn_gpu
 else:
-    import flash_attn_2_cuda as flash_attn_gpu
+    try:
+        import flash_attn_2_cuda as flash_attn_gpu
+    except ImportError:
+        pass
 
 # isort: on
 
