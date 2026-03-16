0125368cc4ed5f20a3985dcd6bcabf7e67bd586d
danthe3rd <43445237+danthe3rd@users.noreply.github.com>
2026-03-16 06:12:54 -0700

    Include cute in release

--- setup.py
+++ setup.py
@@ -634,8 +634,6 @@ setup(
             "docs",
             "benchmarks",
             "flash_attn.egg-info",
-            "flash_attn.cute",
-            "flash_attn.cute.*",
         )
     ),
     author="Tri Dao",
